15253b32a56a8d600399fab71cfca8bc6cd057cb
Trần Quốc Huy <79400264+huytran298@users.noreply.github.com>
2024-08-21 17:06:06 +0000

    code Accepted A to C contest 2001

---
+++ C++/Codeforces/Contest/2001/a.cpp
@@ -0,0 +1,64 @@
+// time-limit: 3000
+/*
+**********************************
+ Author : Akkotsu / huyhuyne         
+**********************************
+*/
+#include<bits/stdc++.h>
+using namespace std; 
+
+#define ll long long
+#define maxN (ll) 1e6 + 5
+#define endl '\n'
+#define fi first
+#define se second
+#define infi (int)(1e18)
+#define bend(a) (a).begin(), (a).end()
+#define rbend(a) (a).rbegin(), (a).rend()
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define LIMIT (long long) 1e18
+#define maxSelf(a, b) a = max(a, b)
+#define minSelf(a, b) a = min(a, b)
+#define elif else if
+#define FORi(a, b, c) for(int i = a; i <= b; i += c)
+#define FORj(a, b, c) for(int j = a; j <= b; j += c)
+#define foreach(a) for(auto x : a)
+#define pb push_back
+#define mod 1000000007
+int nxt(){ int n; cin >> n; return n;}
+/*
+ _  _             _____              
+| || |_  _ _  _  |_   _| _ __ _ _ _  
+| __ | || | || |   | || '_/ _` | ' \ 
+|_||_|\_,_|\_, |   |_||_| \__,_|_||_|
+           |__/                      
+*/
+void solve(){
+    ll n;
+    cin >> n;
+    ll a[n + 1];
+    for(int i = 1; i <= n; i ++)cin >> a[i];
+    vector<ll> cnt(n + 1, 0);
+    ll ans = 0;
+    set<ll> ct;
+    for(int i = 1; i <= n; i ++){
+        cnt[a[i]] ++;
+        ct.insert(a[i]);
+        ans = max(ans, cnt[a[i]]);
+    }
+    cout << ((n == 1 || ct.size() == 1)? 0 : n - ans);
+    
+
+}
+
+signed main(){
+    fast; 
+    ll t = 1;
+    cin >> t;
+    while(t --) {
+        solve();
+        cout << endl;
+    }
+}
