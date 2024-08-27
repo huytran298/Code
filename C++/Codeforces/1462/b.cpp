583516f53d8531c1e6309025c9c2617055db5ccc
Trần Quốc Huy <79400264+huytran298@users.noreply.github.com>
2024-08-27 04:16:52 +0000

    commit again

---
+++ C++/Codeforces/1462/b.cpp
@@ -0,0 +1,69 @@
+// time-limit: 3000
+/*
+**********************************
+ Author : Akkotsu / huyhuyne         
+**********************************
+*/
+#include<bits/stdc++.h>
+using namespace std; 
+
+#define ll long long
+#define maxN (ll) 1e6 + 5
+#define endl '\n'
+#define fi first
+#define se second
+#define infi (int)(1e18)
+#define bend(a) (a).begin(), (a).end()
+#define rbend(a) (a).rbegin(), (a).rend()
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define LIMIT (long long) 1e18
+#define maxSelf(a, b) a = max(a, b)
+#define minSelf(a, b) a = min(a, b)
+#define elif else if
+#define FORi(a, b, c) for(int i = a; i <= b; i += c)
+#define FORj(a, b, c) for(int j = a; j <= b; j += c)
+#define foreach(a) for(auto x : a)
+#define pb push_back
+#define mod 1000000007
+int nxt(){ int n; cin >> n; return n;}
+/*
+ _  _             _____              
+| || |_  _ _  _  |_   _| _ __ _ _ _  
+| __ | || | || |   | || '_/ _` | ' \ 
+|_||_|\_,_|\_, |   |_||_| \__,_|_||_|
+           |__/                      
+*/
+void solve(){
+    string want = "2020";
+    ll n;
+    cin >> n;
+    string inp;
+    cin >> inp;
+    ll l = 0, r = 3;
+    vector<ll> sL, sR;
+    for(int i = 0; i < 4; i ++){
+        if(inp[i] == want[l])l ++;
+        else break;
+        //if(inp[n - i + 1] == want[r])r --;
+
+    }     
+    if(l == 4)return void (cout << "YES");
+    for(int i = 0; i < 4; i ++){
+        if(inp[n - (i + 1)] == want[r])r --;
+        else break;
+    }  
+    if(4 - r - 1 == 4 || l + (4 - r - 1) >= 4)return void (cout << "YES");
+    else cout << "NO";
+}
+
+signed main(){
+    fast; 
+    ll t = 1;
+    cin >> t;
+    while(t --) {
+        solve();
+        cout << endl;
+    }
+}
