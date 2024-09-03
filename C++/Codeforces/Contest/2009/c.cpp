0aa7de7675e6fec4774810dac9b851aee3629c75
Trần Quốc Huy <79400264+huytran298@users.noreply.github.com>
2024-09-03 16:09:28 +0000

    accepted 2009 a - c

---
+++ C++/Codeforces/Contest/2009/c.cpp
@@ -0,0 +1,57 @@
+// time-limit: 3000
+/*
+**********************************
+ Author : Akkotsu / huyhuyne         
+**********************************
+*/
+#include<bits/stdc++.h>
+using namespace std; 
+
+#define ll long long
+#define maxN (ll) 1e6 + 5
+#define endl '\n'
+#define fi first
+#define se second
+#define infi (int)(1e18)
+#define bend(a) (a).begin(), (a).end()
+#define rbend(a) (a).rbegin(), (a).rend()
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define LIMIT (long long) 1e18
+#define maxSelf(a, b) a = max(a, b)
+#define minSelf(a, b) a = min(a, b)
+#define elif else if
+#define FORi(a, b, c) for(int i = a; i <= b; i += c)
+#define FORj(a, b, c) for(int j = a; j <= b; j += c)
+#define foreach(a) for(auto x : a)
+#define pb push_back
+#define mod 1000000007
+int nxt(){ int n; cin >> n; return n;}
+/*
+ _  _             _____              
+| || |_  _ _  _  |_   _| _ __ _ _ _  
+| __ | || | || |   | || '_/ _` | ' \ 
+|_||_|\_,_|\_, |   |_||_| \__,_|_||_|
+           |__/                      
+*/
+void solve(){
+    ll x, y, k;
+    cin >> x >> y >> k;
+    ll xs = x / k, ys = y / k;
+    if(x % k != 0)xs ++;
+    if(y % k != 0)ys ++;
+    if(xs - 1 >= ys)cout << xs * 2 - 1;
+    else cout << ys * 2;
+
+}
+
+signed main(){
+    fast; 
+    ll t = 1;
+    cin >> t;
+    while(t --) {
+        solve();
+        cout << endl;
+    }
+}
