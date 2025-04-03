e21049f64698e54884425170fb502d35b84b207b
huytran298 <huytranquoc298@gmail.com>
2025-04-03 10:06:26 +0700

    just commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,63 +1,28 @@
-#include<bits/stdc++.h>
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-#define ll long long
-#define maxN (long long) 1e6 + 5
-#define endl '\n'
-#define fi first
-#define se second
-#define infi (int)(1e18)
-#define bend(a) (a).begin(), (a).end()
-#define rbend(a) (a).rbegin(), (a).rend()
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) ((a / gcd(a, b)) * b)
-#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define LIMIT (long long) 1e18
-#define maxSelf(a, b) a = max(a, b)
-#define minSelf(a, b) a = min(a, b)
-#define elif else if
-#define FORi(a, b, c) for(int i = a; i <= b; i += c)
-#define FORj(a, b, c) for(int j = a; j <= b; j += c)
-#define foreach(a) for(auto x : a)
-#define pb push_back
-#define mod 1000000007
-int nxt(){ int n; cin >> n; return n;}
-/*
- _  _             _____              
-| || |_  _ _  _  |_   _| _ __ _ _ _  
-| __ | || | || |   | || '_/ _` | ' \ 
-|_||_|\_,_|\_, |   |_||_| \__,_|_||_|
-           |__/                      
-*/
-
-signed main(){
-    fast; 
-    ll n, r, c;
-    cin >> n >> r >> c;
-    string s;
-    cin >> s;
-    map<pair<ll, ll>, ll> mp;
-    cout << s;
-    return 0;
-    ll posX, posY;
-    posX = posY = 0;
-    for(char x : s){
-        mp[{posX, posY}] = 1;
-        if(x == 'N'){
-            posY ++;
-            r ++;
-        }else if(x == 'S'){
-            posY --;
-            r --;
-        }else if(x == 'E'){
-            posX --;
-            c --;
-        }else{
-            posX --;
-            c --;
-        }
-        if(mp[{c, r}]){
-            cout << 1;
-        }else cout << 0;
+int sumArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
     }
+    return sum;
+}
+double averageArray(int arr[], int size) {
+    return static_cast<double>(sumArray(arr, size)) / size;
+}
+int main() {
+    int size;
+    cout << "Enter the number of elements in the array: ";
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Sum of array elements: " << sumArray(arr, size) << endl;
+    cout << "Average of array elements: " << averageArray(arr, size) << endl;
+    return 0;
 }
