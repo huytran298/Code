0652ff724adb0551b9f431728fd473e81f091160
huytran298 <huytranquoc298@gmail.com>
2025-08-13 00:59:06 +0700

    Refactor test.py to remove sound playback and add time checks with remaining seconds calculation

--- Python/test.py
+++ Python/test.py
@@ -1,3 +1,11 @@
-from playsound import playsound
+import time, datetime
 
-playsound('/Users/huytranquoc/Documents/Code/Python/Oscilloscope/sound.mp3')
+timeNow = datetime.datetime.now().time()
+print(timeNow)
+if timeNow >= datetime.time(7,0,0) and datetime.time(15,0,0) :
+    print("YES")
+else :
+    print("NO")
+secs = (15 * 3600) - (timeNow.hour * 3600 + timeNow.minute * 60 + timeNow.second)
+
+print(secs)
