d57b90e8b1f24fc92d59435b1338600fef44957f
huytran298 <huytranquoc298@gmail.com>
2026-01-07 11:39:36 +0700

    ;v

--- C++/tempCodeRunnerFile.cpp
+++ C++/tempCodeRunnerFile.cpp
@@ -1,12 +1,52 @@
-#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std; 
 
-int main(){
-    int a;
-    scanf("%d", &a);
-    printf("%d", a);
-    //string %s
-    // float %f
-    //double %d
-    // int %d %l
+#define ll long long
+#define maxN (long long) 1e6 + 5
+#define endl '\n'
+#define fi first
+#define se second
+#define infi (int)(1e18)
+#define bend(a) (a).begin(), (a).end()
+#define rbend(a) (a).rbegin(), (a).rend()
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define LIMIT (long long) 1e18
+#define maxSelf(a, b) a = max(a, b)
+#define minSelf(a, b) a = min(a, b)
+#define elif else if
+#define FORi(a, b, c) for(int i = a; i <= b; i += c)
+#define FORj(a, b, c) for(int j = a; j <= b; j += c)
+#define foreach(a) for(auto x : a)
+#define pb push_back
+#define mod 1000000007
+int nxt(){ int n; cin >> n; return n;}
+/*
+ _  _             _____              
+| || |_  _ _  _  |_   _| _ __ _ _ _  
+| __ | || | || |   | || '_/ _` | ' \ 
+|_||_|\_,_|\_, |   |_||_| \__,_|_||_|
+           |__/                      
+*/
+
+signed main(){
+    fast; 
+    ll c[6] ;
+    c[0] = -2;
+    c[1] = 1;
+    for(int z = 1; z <= 5; z ++){
+        ll ctemp[6];
+
+        for(int diff = 0; diff <= 5; diff ++){
+            for(int l = -5; l <= 5; l ++){
+                ctemp[diff] += c[abs(diff + l)];
+            }
+            for(ll k = 0; k <= 5; k ++){
+                c[k] = ctemp[k];
+            }
+        }
+    }
+    for(int i = 0; i < 6; i ++)cout << c << ' ';
     
 }
